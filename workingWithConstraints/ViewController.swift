0295b0782580693497e740b151c519cc6bec3fe6
Maria Mamina <maria.mamina3@gmail.com>
2021-01-26 12:01:00 +0200

    Initial Commit

---
+++ workingWithConstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workingWithConstraints
+//
+//  Created by Maria Mamina on 26.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
